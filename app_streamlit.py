0104369bd74d74909452cd7d5cb520527c58ee04
MF-KIMPOEL <igelvolker93@gmail.com>
2025-09-01 10:16:45 +0700

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -2,19 +2,19 @@ import streamlit as st
 import pandas as pd
 import joblib
 
-model=joblib.load("model_mlbb.joblib")
+model = joblib.load("model_mlbb.joblib")
 
 st.title("MLBB Classification")
-kill = st.slider("jumlah kill",0,20)
-assit = st.slider("jumlah assist",0,20)
-death = st.slider("jumlah death",0,20)
-turret = st.slider("jumlah turret",0,20)
+kill = st.slider("jumlah kill", 0, 20)
+assist = st.slider("jumlah assist", 0, 20)   # fix typo 'assit' -> 'assist'
+death = st.slider("jumlah death", 0, 20)
+turret = st.slider("jumlah turret", 0, 20)
 
-id st.button("predict"):
-	data_baru = pd.DataFrame([[kill,assist,death,turret]],
-		columns=["kill","assist","death","turret"])
-	st.success(f"hasil prediksi : {model.predict(data_baru)[0]}")
-	st.balloons()
+if st.button("predict"):   # fix 'id' -> 'if'
+    data_baru = pd.DataFrame([[kill, assist, death, turret]],
+        columns=["kill", "assist", "death", "turret"])
+    st.success(f"hasil prediksi : {model.predict(data_baru)[0]}")
+    st.balloons()
 
 # simpan di folder classification
 # buka CMD baru
