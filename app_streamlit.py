6caf733ef34c6de3d05b448469dc273c89c38e72
MF-KIMPOEL <igelvolker93@gmail.com>
2025-09-01 10:10:34 +0700

    Add files via upload

---
+++ app_streamlit.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+import joblib
+
+model=joblib.load("model_mlbb.joblib")
+
+st.title("MLBB Classification")
+kill = st.slider("jumlah kill",0,20)
+assit = st.slider("jumlah assist",0,20)
+death = st.slider("jumlah death",0,20)
+turret = st.slider("jumlah turret",0,20)
+
+id st.button("predict"):
+	data_baru = pd.DataFrame([[kill,assist,death,turret]],
+		columns=["kill","assist","death","turret"])
+	st.success(f"hasil prediksi : {model.predict(data_baru)[0]}")
+	st.balloons()
+
+# simpan di folder classification
+# buka CMD baru
+# jalankan virtual environment lagi
+# jalankan dengan perintah : streamlit run app_streamlit.py
